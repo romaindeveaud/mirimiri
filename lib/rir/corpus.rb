bc18b961bdd99fd9f3bb51dc8fd6e88757de3eb8
Romain Deveaud <romain.deveaud@gmail.com>
2010-11-23 18:21:37 +0100

    more doc

--- lib/rir/corpus.rb
+++ lib/rir/corpus.rb
@@ -17,7 +17,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# General module for many purposes related to Information Retrieval.
 module RIR
 
   class Corpus
